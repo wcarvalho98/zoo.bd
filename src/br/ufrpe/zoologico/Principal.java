20826e3732898e5294dd50cc3542a950df65fd55
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2018-01-15 11:39:37 -0300

    First class!

---
+++ src/br/ufrpe/zoologico/Principal.java
@@ -0,0 +1,20 @@
+/**	Zoológico - BD - 15/01/2018
+ * Classe de início do projeto!
+ */
+package br;
+
+/**
+ * @author Wilder
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

526f5e528f07559ca859e208029bd9c2c1627227
Pedro <Pedro@10.246.227.101>
2018-01-17 09:53:50 -0300

    besteirinhas

---
+++ src/br/ufrpe/zoologico/negocio/Docs.java
@@ -0,0 +1,11 @@
+/** 
+ * @author Pedro
+ * UFRPE - Banco de dados
+ * br.ufrpe.zoologico.negocio - Versao 1.0 - 2017.2
+ * TODO 16.01.2018 
+ */
+package br.ufrpe.zoologico.negocio;
+
+public class Docs {
+
+}
---
+++ src/br/ufrpe/zoologico/negocio/Fones.java
@@ -0,0 +1,41 @@
+/** 
+ * @author Pedro
+ * UFRPE - Banco de dados
+ * br.ufrpe.zoologico.negocio - Versao 1.0 - 2017.2
+ * TODO 16.01.2018 
+ */
+package br.ufrpe.zoologico.negocio;
+
+public class Fones {
+	private String fone;
+	private Integer idZoo;
+	/**
+	 * Metodo: getFone
+	 * @return fone
+	 */
+	public String getFone() {
+		return fone;
+	}
+	/**
+	 * Metodo: setFone
+	 * @param fone the fone to set
+	 */
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	/**
+	 * Metodo: getIdZoo
+	 * @return idZoo
+	 */
+	public Integer getIdZoo() {
+		return idZoo;
+	}
+	/**
+	 * Metodo: setIdZoo
+	 * @param idZoo the idZoo to set
+	 */
+	public void setIdZoo(Integer idZoo) {
+		this.idZoo = idZoo;
+	}
+	
+}

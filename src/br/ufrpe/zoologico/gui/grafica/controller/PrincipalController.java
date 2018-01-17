548822fc2bcdfd9a5857f02ffdf0c6c8ccec50a8
Wilder <Escritrio@DESKTOP-VMTI0NN>
2018-01-17 00:40:37 -0300

    Algumas novidades :p

--- src/br/ufrpe/zoologico/gui/grafica/controller/PrincipalController.java
+++ src/br/ufrpe/zoologico/gui/grafica/controller/PrincipalController.java
@@ -6,6 +6,7 @@
  */
 package br.ufrpe.zoologico.gui.grafica.controller;
 
+import java.awt.Event;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,7 +14,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.stage.WindowEvent;
 
 public class PrincipalController implements Initializable{
@@ -22,12 +25,40 @@ public class PrincipalController implements Initializable{
 	private TextField loginId;
 	@FXML
 	private PasswordField senhaId;
+	@FXML
+	private ProgressIndicator progressId;
+	@FXML
+	private Tooltip tooltipLogin;
+	@FXML
+	private Tooltip tooltipSenha;
 
 	private Telas t;
+	
+	@FXML
+	public void validaLogin() {
+		String usuario = loginId.getText();
+		String senha = senhaId.getText();
+		
+		if (usuario.isEmpty())
+			tooltipLogin.show(t.getCena().get(0).getWindow());
+		else
+			tooltipLogin.hide();
+		if (senha.isEmpty())
+			tooltipSenha.show(t.getCena().get(0).getWindow());
+		else
+			tooltipSenha.hide();
+		if (!senha.isEmpty() && !usuario.isEmpty()) {
+			progressId.setVisible(true);
+			loginId.setEditable(false);
+			senhaId.setEditable(false);
+		}
+			
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		t = Telas.getInstance();
+		t.getDialogStage().setResizable(false);
 		t.getDialogStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
 
 			@Override

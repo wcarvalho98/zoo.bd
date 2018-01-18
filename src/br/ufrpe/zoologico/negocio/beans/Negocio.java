61078a6e32b6ff3d4845a3ee3f9a83f4669ae6ea
IgorSouza21 <igor.rgsouza@ufrpe.br>
2018-01-18 17:06:35 -0300

    Adicionando elementos para conexão com o banco

--- src/br/ufrpe/zoologico/negocio/beans/Negocio.java
+++ src/br/ufrpe/zoologico/negocio/beans/Negocio.java
@@ -4,7 +4,7 @@
  * br.ufrpe.zoologico.negocio - Versao 1.0 - 2017.2
  * TODO 15.01.2018 
  */
-package br.ufrpe.zoologico.negocio;
+package br.ufrpe.zoologico.negocio.beans;
 
 public class Negocio {
 
---
+++ src/br/ufrpe/zoologico/util/ConFactory.java
@@ -0,0 +1,17 @@
+package br.ufrpe.zoologico.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConFactory {
+	
+	private static final String MySQLDriver = "com.mysql.jdbc.Driver";
+	
+	public static Connection conexao(String url, String nome, String senha) throws ClassNotFoundException, SQLException{
+
+		Class.forName(MySQLDriver);
+		return DriverManager.getConnection(url, nome, senha);
+	}
+
+}

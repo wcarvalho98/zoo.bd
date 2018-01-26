4a637c1692024643c7d327ae047898cdaf1a9047
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2018-01-26 02:43:29 -0300

    Função feita mas tem coisa pra fazer ainda...

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -8,5 +8,22 @@ create definer = `roat`@`localhost` function oferta (idServico int, prazo date) 
 	else
 		return false;
 	end if;
- end$$
- delimiter ;
+ end
+$$
+
+delimiter $$
+create function disponivel_espaco (`id_espaco` int) returns date
+ begin
+	declare `retorno` date;
+	declare `dia_disponivel` date default curdate();
+	select dt_reserva into `retorno` from reserva where `id_espaco` = reserva.id_espaco;
+	if `dia_disponivel`!= `retorno` then
+		return `dia_disponivel`;
+	else 
+		while `dia_disponivel` = `retorno` do
+			set `dia_disponivel` = adddate(`dia_disponivel`, 1);
+		end while;
+		return `dia_disponivel`;
+	end if;
+ end
+$$

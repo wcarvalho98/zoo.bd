1c6cb35b218b531aac51b5db6831bc0e707a42a0
LAPTOP-C7D0LLR1\Wilder <wcarvalho98@gmail.com>
2018-01-26 00:34:06 -0300

    O script.sql é pra função, trigger e procedures!

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,13 +1,3 @@
-CREATE TRIGGER insere_fatura AFTER INSERT INTO fatura FOR EACH row
-BEGIN
-	insert into nota_fiscal
-	set 
-	ICMS = (new.valor * 0.09547),
-	valor_total = new.valor + ICMS,
-	vl_total_desconto = valor_total * 0.9,
-	data_NF = now();
-END;
-
 delimiter $$
 create definer = `roat`@`localhost` function oferta (idServico int, prazo date) returns bool
  begin
